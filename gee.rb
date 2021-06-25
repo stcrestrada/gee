5b8545e65aa81446cbf8a1f6b952f4a7de5dd25a
Stephen Estrada <sestrada@streetshares.com>
2021-06-24 21:41:26 -0400

    copy over gee.rb

--- gee.rb
+++ gee.rb
@@ -4,27 +4,4 @@ class Gee < Formula
   url "https://github.com/stcrestrada/gee/archive/refs/tags/v0.1.8.tar.gz"
   sha256 "03c43ff2aa8e604345cfdd754863a6db5511b98c6ce794d8c487ae8f23f14ec7"
   license "MIT"
-
-  # depends_on "cmake" => :build
-
-  def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gee`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
