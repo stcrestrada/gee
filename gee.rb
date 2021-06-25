19876480198a34073ff10822914bc99129f41040
Stephen Estrada <sestrada@streetshares.com>
2021-06-24 21:56:48 -0400

    update goreleaser config

--- gee.rb
+++
@@ -1,7 +0,0 @@
-class Gee < Formula
-  desc "Project Gee"
-  homepage "https://github.com/stcrestrada/gee"
-  url "https://github.com/stcrestrada/gee/archive/refs/tags/v0.1.10.tar.gz"
-  sha256 "86796b9fccea4ba6241f3357a5658c3faf103ebad1d641e093549923a336c317"
-  license "MIT"
-end
